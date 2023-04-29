73a94e66db1815d9170c8580364eccab7d7368e8
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-29 16:12:09 +0600

    create layout, header components and moments js

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is Header</h1>
+            <div className="text-center">
+                <img src={logo} alt="" />
+                <p className="text-secondary">
+                    <small>Journalism Without Fear or Favour</small>
+                </p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
         </div>
     );
 };
---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const LeftNav = () => {
+    return (
+        <div>
+            <h2>Left Nav</h2>
+        </div>
+    );
+};
+
+export default LeftNav;
