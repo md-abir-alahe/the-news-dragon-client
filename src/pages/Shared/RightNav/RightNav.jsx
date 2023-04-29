3396739a3cf5dd3b39a9d7a755169df90a00694b
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-29 17:39:37 +0600

    implement right side component and icons

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,38 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h3>Right Nav</h3>
+            <div>
+                <h4>Login With</h4>
+                <Button className='mb-2' variant="outline-primary"> <FcGoogle /> Login with Google</Button>
+                <Button variant="outline-secondary"> <FaGithub /> Login with Github</Button>
+            </div>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <div>
+                    <img src={bg} alt="" />
+                    <div>
+                        <h3>Create an Amazing Newspaper</h3>
+                        <p>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+                        <Button variant="danger">Learn More</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
