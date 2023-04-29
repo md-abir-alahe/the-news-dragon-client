a2fe51d654eff657306397696af4b1e0967537f3
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-29 12:27:50 +0600

    project setup and router setup

---
+++ src/firebase/firbase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGvxNy6KzXHsgEagRIv1aZnmysz59HIWk",
+  authDomain: "the-news-dragon-ff3b1.firebaseapp.com",
+  projectId: "the-news-dragon-ff3b1",
+  storageBucket: "the-news-dragon-ff3b1.appspot.com",
+  messagingSenderId: "796263753444",
+  appId: "1:796263753444:web:7cef97b5f6c4d708efaef1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is Header</h1>
+        </div>
+    );
+};
+
+export default Header;
