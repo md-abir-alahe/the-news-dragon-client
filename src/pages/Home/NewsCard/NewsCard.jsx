6c2ab4abc2df967a337d146413ec487a50075662
md-abir-alahe <mdabiralahe@gmail.com>
2023-05-02 03:03:12 +0600

    server initial setup done

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 import { Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaBookmark, FaEye, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
-import Rating from 'react-rating';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const NewsCard = ({ news }) => {
     const { _id, title, details, image_url, author, total_view, rating } = news;
@@ -38,15 +39,12 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
             </Card.Body>
             <Card.Footer className="text-muted d-flex">
-                <div className='flex-grow-1'>
+                <div className='flex-grow-1 d-flex align-items-center'>
                     <Rating
-                        placeholderRating={rating?.number}
-                        readonly
-                        emptySymbol={<FaRegStar></FaRegStar>}
-                        placeholderSymbol={<FaStar className='text-warning'></FaStar>}
-                        fullSymbol={<FaStar></FaStar>}
-                    />
-                    <span>{rating?.number}</span>
+                        style={{ maxWidth: 150 }}
+                        value={rating?.number}
+                        readOnly />
+                    <span className='ms-2'>{rating?.number}</span>
                 </div>
                 <div>
                     <FaEye></FaEye> {total_view}
